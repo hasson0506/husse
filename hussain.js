2725fa5d1b41a435f9d3732f6c558bbfdaa0ad34
Hussain0506 <112064132+Hussain0506@users.noreply.github.com>
2022-08-29 12:34:03 +0200

    hello

---
+++ hussain.js
@@ -0,0 +1,8 @@
+console.log ("hello world")
+console.log ("Once upon a time")
+console.log ("I was falling in love")
+console.log ("Now I’m only falling apart")
+console.log("Once upon a timeI was falling in love Now I’m only falling apart")
+var text = "Trip and food!";
+console.log(message)
+console.log("var name = "Ada Lovelace";")
